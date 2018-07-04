fbf290377d1a49207b6cfa3ac46eef84d6a44d11
marcinsulowski <mr.sulowski@gmail.com>
2018-07-04 18:39:41 +0200

    product card bug fixed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,28 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ProductCard = props => {
-  const { products } = props;
+  const { product } = props;
 
   return (
-    <div className="products-container">
-      {products.map(product => (
-        <figure className="product" key={`product-${product.id}`}>
-          <img
-            className="product__image"
-            src={product.image}
-            alt={product.name}
-          />
-          <figcaption className="product__caption">{product.name}</figcaption>
-          <p className="product__price">{product.price} PLN</p>
-          <button className="btn btn--cart">Add to cart</button>
-        </figure>
-      ))}
-    </div>
+    <figure className="product">
+      <img className="product__image" src={product.image} alt={product.name} />
+      <figcaption className="product__caption">{product.name}</figcaption>
+      <p className="product__price">{product.price} PLN</p>
+      <button className="btn btn--cart">Add to cart</button>
+    </figure>
   );
 };
 
 ProductCard.propTypes = {
-  products: PropTypes.array.isRequired,
+  product: PropTypes.object.isRequired,
 };
 
 export default ProductCard;
