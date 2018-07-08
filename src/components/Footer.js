f190e0199e8f48a6dc7c7148c930dfd3e9b0817e
marcinsulowski <mr.sulowski@gmail.com>
2018-07-08 23:42:56 +0200

    footer nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,17 @@ const Footer = () => {
       <div className="vinyl">
         <div className="vinyl__inner" />
       </div>
-      <p className="footer__text">Copyright &#169; Marcin Sulowski</p>
+
+      <nav className="footer-nav">
+        <h3 className="footer-nav__title">Copyright &#169; Marcin Sulowski</h3>
+        <a className="footer-nav__link" href="https://github.com/MarcinSulowski/record-shop">
+          <span className="footer-nav__dash">– </span>See source code<span className="footer-nav__dash"> –</span>
+        </a>
+        <a className="footer-nav__link" href="mailto:mr.sulowski@gmail.com">
+        <span className="footer-nav__dash">– </span>Contact me<span className="footer-nav__dash"> –</span>
+        </a>
+      </nav>
+
       <div className="vinyl">
         <div className="vinyl__inner" />
       </div>
