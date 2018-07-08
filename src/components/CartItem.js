c3d6a1c37ff4076d8edd80c60a87423133693b6b
marcinsulowski <mr.sulowski@gmail.com>
2018-07-08 23:42:31 +0200

    fixed bug with displaying total amount

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,65 +6,61 @@ class CartItem extends React.Component {
     super(props);
 
     this.state = {
-      quantity: this.props.quantity,
+      item: this.props.item,
     };
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
-    this.updateOnChange = this.updateOnChange.bind(this);
+    this.updateInputOnChange = this.updateInputOnChange.bind(this);
   }
 
   increment() {
-    this.setState(
-      prevState => ({ quantity: Number(prevState.quantity) + 1 }),
-      this.props.updateQuantity(this.state.quantity)
-    );
+    const { item, updateQuantity } = this.props;
+
+    item.quantity += 1;
+    updateQuantity();
   }
 
   decrement() {
-    if (this.state.quantity > 1) {
-      this.setState(
-        prevState => ({ quantity: Number(prevState.quantity) - 1 }),
-        this.props.updateQuantity(this.state.quantity)
-      );
+    const { item, updateQuantity } = this.props;
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateQuantity();
     }
   }
 
-  // updates quantity on input change
-  updateOnChange() {
-    this.setState(
-      { quantity: this.refs.qty.value },
-      this.props.updateQuantity(this.state.quantity)
-    );
+  updateInputOnChange() {
+    const { item, updateQuantity } = this.props;
+
+    item.quantity = Number(this.refs.qtyInp.value);
+    updateQuantity();
   }
 
   render() {
     const { item, removeItem } = this.props;
-
     return (
       <div className="cart-item">
         <img className="cart-item__img" src={item.image} alt="" />
         <p className="cart-item__caption">{item.name}</p>
         <div className="stepper">
           <input
-            ref="qty"
+            ref="qtyInp"
             type="number"
             className="stepper__input"
-            value={this.state.quantity}
-            onChange={this.updateOnChange}
+            value={item.quantity}
+            onChange={this.updateInputOnChange}
           />
           <div className="stepper__btns">
-          <button className="stepper__btn btn" onClick={this.increment}>
-            +
-          </button>
-          <button className="stepper__btn btn" onClick={this.decrement}>
-            –
-          </button>
+            <button className="stepper__btn btn" onClick={this.increment}>
+              +
+            </button>
+            <button className="stepper__btn btn" onClick={this.decrement}>
+              –
+            </button>
           </div>
         </div>
-        <p className="cart-item__price">
-          {this.state.quantity * item.price}PLN
-        </p>
+        <p className="cart-item__price">{item.quantity * item.price} PLN</p>
         <button
           className="cart__remove-btn btn"
           onClick={() => removeItem(item)}
@@ -79,7 +75,6 @@ class CartItem extends React.Component {
 CartItem.propTypes = {
   item: PropTypes.object.isRequired,
   removeItem: PropTypes.func.isRequired,
-  quantity: PropTypes.number.isRequired,
   updateQuantity: PropTypes.func.isRequired,
 };
 
